fd24f7f9c4704fd01b217a846d84c7d4a6edbb16
Salim Blume <salim@runsafesecurity.com>
2024-03-13 13:11:17 -0400

    Moving where schema is defined and adding debug

--- index.d.ts
+++ index.d.ts
@@ -18,6 +18,7 @@ interface SequelizeStoreOptions {
   extendDefaultFields?: (defaults: DefaultFields, session: any) => Data;
   checkExpirationInterval?: number;
   expiration?: number;
+  schema?: string;
 }
 
 declare class SequelizeStore extends Store {
